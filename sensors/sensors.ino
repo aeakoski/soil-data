26d2551b377a787c7719d8f9398406c6da0f086f
akoski <akoski@kth.se>
2021-07-11 13:41:25 +0200

    Use better temperature sensors

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,13 +1,25 @@
 #include <dht.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 dht DHT;
-#define DHT11_PIN 6
+#define DHT11_PIN A1
 
-int WHEAT_SOIL_MOISTURE_PIN = 0; // Soil Sensor input at Analog PIN A0
+int RYE_SOIL_MOISTURE_PIN = 0; // Soil Sensor input at Analog PIN A0
+int RYE_SOIL_TEMPERATURE_PIN = A2; // Soil Temperature input at Analog PIN A2
+int AIR_TEMPERATURE_PIN = A3; // Soil Temperature input at Analog PIN A3
+
+OneWire ryeSoilWire(RYE_SOIL_TEMPERATURE_PIN);
+OneWire airWire(AIR_TEMPERATURE_PIN);
+DallasTemperature ryeSoilSensor(&ryeSoilWire);
+DallasTemperature airTemperatureSensor(&airWire);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("");
+  ryeSoilSensor.begin();
+  airTemperatureSensor.begin();
+  delay(2000);
 }
 
 void metricToSerial(String location, String type, String unit, float measurement){
@@ -15,17 +27,28 @@ void metricToSerial(String location, String type, String unit, float measurement
 }
 
 void meassureSoilMoisture(){
-  float value;
-  value = analogRead(WHEAT_SOIL_MOISTURE_PIN)/10.0;
-  metricToSerial("WHEAT","SOIL-MOISTURE","PERCENT", value);
+  float value = 0.0;
+  for(int i = 0; i<=100; i++){
+    value = value + (analogRead(RYE_SOIL_MOISTURE_PIN)/10.0);
+  }
+  value = value/100.0;
+  metricToSerial("RYE","SOIL-MOISTURE","PERCENT", value);
 }
 
-void meassureSoilTemperature(){}
+void meassureSoilTemperature(){
+  ryeSoilSensor.requestTemperatures(); // Send the command to get temperatures
+  if(ryeSoilSensor.getTempCByIndex(0) < -50){ return; }
+  metricToSerial("RYE","SOIL-TEMPERATURE","CELSIUS", ryeSoilSensor.getTempCByIndex(0));
+}
+
 void meassureAirTemperature(){
-  metricToSerial("FIELD","AIR-TEMPERATURE","CELSIUS", DHT.temperature);
+  airTemperatureSensor.requestTemperatures(); // Send the command to get temperatures
+  if(airTemperatureSensor.getTempCByIndex(0) < -50){ return; }
+  metricToSerial("FIELD","AIR-TEMPERATURE","CELSIUS", airTemperatureSensor.getTempCByIndex(0));
 }
 void meassureAirMoisture(){
-  metricToSerial("FIELD","AIR-MOISTURE","PERCENT", DHT.humidity);  
+  if(DHT.humidity < 0){ return; }
+  metricToSerial("FIELD","AIR-MOISTURE","PERCENT", DHT.humidity);
 }
 void meassureSunlight(){}
 
@@ -34,6 +57,7 @@ void loop() {
  meassureSoilMoisture();
  meassureAirTemperature();
  meassureAirMoisture();
+ meassureSoilTemperature();
  delay(5000);
 
 }
